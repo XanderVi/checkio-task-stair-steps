d2f13009342b40a3690bf6c6391601f425d4f498
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-21 10:26:06 +1200

    tests and referee

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,26 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
+            'answer': 6, 'explanation': [1, 0, 1, 1], 'input': [5, -3, -1, 2]},
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            'answer': 8, 'explanation': [1, 1, 0, 1, 1], 'input': [5, 6, -10, -7, 4]},
+        {
+            'answer': 393, 'explanation': [0, 1, 1, 0, 1, 1, 1, 1, 0, 1], 'input': [-11, 69, 77, -51, 23, 67, 35, 27, -25, 95]},
+        {
+            'answer': 125, 'explanation': [0, 1, 0, 1, 1, 1, 1, 1, 1], 'input': [-21, -23, -69, -67, 1, 41, 97, 49, 27]},
+        {
+            'answer': 14, 'explanation': [0, 1, 0, 1, 0, 1, 0, 1, 1, 1], 'input': [-47, 13, -39, -97, -85, -23, -11, 89, 15, 17]},
+        {
+            'answer': 5, 'explanation': [1, 1, 1, 1, 1], 'input': [1, 1, 1, 1, 1]},
+        {
+            'answer': 20, 'explanation': [0, 1, 0, 1], 'input': [-10, 10, -10, 10]}
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
+            'input': [13, 24, -23, -12, -12, 12, 13], 'explanation': [1, 1, 0, 1, 0, 1, 1], 'answer': 50},
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+            'input': [-10, -10, -10, 100, -10, -10], 'explanation': [0, 1, 0, 1, 0, 1], 'answer': 80},
+        {
+            'input': [5, 4, 3, -100, 2, -20], 'explanation': [1, 1, 1, 0, 1, 0], 'answer': 14},
     ]
 }
